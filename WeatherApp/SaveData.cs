a85f33ef827d1536eb9394ce5a235571b344918b
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-01-06 11:06:24 +0100

    translator well done (not finished)

---
+++ WeatherApp/SaveData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+namespace Save {
+    public class SaveFile {
+        private const string FileName = "options.json";
+        private Dictionary<string, string> options = new Dictionary<string, string>();
+        public SaveFile() {
+            
+        }
+    }
+}
