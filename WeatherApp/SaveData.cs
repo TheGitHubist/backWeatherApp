fe912e5abfba0c29e111b1acccbbff77afa6f79c
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-01-06 11:15:17 +0100

    add saveData for options.json

--- WeatherApp/SaveData.cs
+++ WeatherApp/SaveData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Save {
@@ -6,7 +7,25 @@ namespace Save {
         private const string FileName = "options.json";
         private Dictionary<string, string> options = new Dictionary<string, string>();
         public SaveFile() {
-            
+            options.Add("city", "London");
+            options.Add("language", "en");
+            File.WriteAllText(FileName, options.ToString());
+        }
+        public void Save(string? city, string? language)
+        {
+            if (city != null)
+            {
+                options["city"] = city;
+            }
+            if (language != null)
+            {
+                options["language"] = language;
+            }
+            if (city == null && language == null)
+            {
+                return;
+            }
+            File.WriteAllText(FileName, options.ToString());
         }
     }
 }
